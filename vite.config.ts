34da282ff2c75ff30469e4aaf74f6d6319c93feb
Zhan,Shi-Xian <samchan720@gmail.com>
2026-02-23 11:25:26 +0800

    add sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'// 1. 引入插件
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
+      
+    }),
+    Sitemap({ 
+      hostname: 'https://tier-list-maker168.netlify.app/', // 替換成你的網址
+      dynamicRoutes: [], // 如果是 SPA，手動列出路由
     }),
     tailwindcss(), // 2. 加入這裡
   ],
