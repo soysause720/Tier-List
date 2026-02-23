5fa72981763f43579223f99ebd70f6e4423b4369
Zhan,Shi-Xian <samchan720@gmail.com>
2026-02-24 01:28:24 +0800

    fix: remove unused useState import in Toast.tsx

--- src/component/Toast.tsx
+++ src/component/Toast.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { AiOutlineCheckCircle, AiOutlineCloseCircle } from "react-icons/ai";
 
 export type ToastType = "success" | "error";
